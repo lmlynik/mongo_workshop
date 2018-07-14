9b28087b2cb49e59653d414ecca0824450f2c06d
lmlynik <lukasz.mlynik@hotmail.com>
2018-07-14 08:21:29 +0200

    Read collection

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,19 @@
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.Block;
+import com.mongodb.client.*;
+import org.bson.Document;
+
+import java.net.UnknownHostException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!"); // Display the string.
+    public static void main(String[] args) throws UnknownHostException {
+        MongoClient mongoClient = MongoClients.create();
+        MongoDatabase database = mongoClient.getDatabase("codementors");
+
+        MongoCollection<Document> users = database.getCollection("users");
+
+        Block<Document> printBlock = document -> System.out.println(document.toJson());
+
     }
 }
